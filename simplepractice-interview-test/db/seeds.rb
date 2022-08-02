9830e716ee56c693663994533a58da5d9b9278be
dair68 <granthuang68@yahoo.com>
2022-08-01 23:34:10 -0700

    seeded 10 doctors

--- simplepractice-interview-test/db/seeds.rb
+++ simplepractice-interview-test/db/seeds.rb
@@ -3,3 +3,42 @@
 # - Each doctor should have 10 patients with unique names
 # - Each patient should have 10 appointments (5 in the past, 5 in the future)
 #   - Each appointment should be 50 minutes in duration
+Doctor.destroy_all
+Doctor.create!(
+[
+    {
+        name: "Florence Nightingale"
+    },
+    {
+        name: "Susan La Flesche Picotte"
+    },
+    {
+        name: "Louis Pasteur"
+    },
+    {
+        name: "Alexander Fleming"
+    },
+    {
+        name: "Mario Mario"
+    },
+    {
+        name: "Phil McGraw"
+    },
+    {
+        name: "Julius Hibbert"
+    },
+    {
+        name: "Sigma Klim"
+    },
+    {
+        name: "Ken Jeong"
+    },
+    {
+        name: "Emily Zarka"
+    }
+])
+
+p "Created #{Doctor.count} doctors"
+Doctor.all.each do |n|
+    p n
+end
