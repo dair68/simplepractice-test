40bc000b54510fe2de7817ee47865a69513e7fa5
dair68 <granthuang68@yahoo.com>
2022-08-19 20:40:14 -0700

    json response for GET api/doctors

--- simplepractice-interview-test/app/controllers/api/doctors_controller.rb
+++ simplepractice-interview-test/app/controllers/api/doctors_controller.rb
@@ -1,11 +1,11 @@
 class Api::DoctorsController < ApplicationController
   # obtains all doctors that don't have an appointment
-  def free
+  def index
     # GET api/doctors
     logger.debug { "Finding doctors without appointments" }  
-    @doctors = Doctor.left_outer_joins(:appointments).where(appointments: {id: nil})
-    logger.debug { "Found #{@doctors.count} doctors with no appointments"}
-    logger.debug { "Sample free doctors: #{@doctors.limit(5).inspect}"}
-    head :ok
+    doctors = Doctor.left_outer_joins(:appointments).where(appointments: {id: nil})
+    logger.debug { "Found #{doctors.count} doctors with no appointments"}
+    logger.debug { "Sample free doctors: #{doctors.limit(5).inspect}"}
+    render :json => doctors, :status => :ok
   end
 end
